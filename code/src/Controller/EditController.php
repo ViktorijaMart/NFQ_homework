2ff2e17595d1d3a2131133eea8c916ea7df3bef5
LhunaCat <viktorijamartinaityte@gmail.com>
2023-04-06 16:51:35 +0300

    added form

--- code/src/Controller/EditController.php
+++ code/src/Controller/EditController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Article;
+use App\Form\ArticleEditType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,11 +12,14 @@ use Symfony\Component\Routing\Annotation\Route;
 class EditController extends AbstractController
 {
     #[Route('/article/edit/{id}', name: 'article_edit')]
-    public function edit(): Response
+    public function edit(EntityManagerInterface $em, int $id): Response
     {
+        $article = $em->getRepository(Article::class)->find($id);
+
+        $form = $this->createForm(ArticleEditType::class, $article);
 
         return $this->render('edit/index.html.twig', [
-            'controller_name' => 'EditController',
+            'form' => $form->createView(),
         ]);
     }
 }
