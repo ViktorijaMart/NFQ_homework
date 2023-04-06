e6e7eb76c7c98976e14af8781b9117d0e1289e28
LhunaCat <viktorijamartinaityte@gmail.com>
2023-04-06 17:05:13 +0300

    added submit button

--- code/src/Form/ArticleEditType.php
+++ code/src/Form/ArticleEditType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\Article;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,6 +16,7 @@ class ArticleEditType extends AbstractType
             ->add('title')
             ->add('text')
             ->add('image')
+            ->add('edit', SubmitType::class)
         ;
     }
 
