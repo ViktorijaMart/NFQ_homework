83a791d0d52989e3205dbf1f8625c3488307331e
LhunaCat <viktorijamartinaityte@gmail.com>
2023-04-07 14:24:58 +0300

    edit controller logic

--- code/src/Controller/EditController.php
+++ code/src/Controller/EditController.php
@@ -6,17 +6,29 @@ use App\Entity\Article;
 use App\Form\ArticleEditType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EditController extends AbstractController
 {
     #[Route('/article/edit/{id}', name: 'article_edit')]
-    public function edit(EntityManagerInterface $em, int $id): Response
+    public function edit(EntityManagerInterface $em, Request $request, int $id): Response
     {
         $article = $em->getRepository(Article::class)->find($id);
 
         $form = $this->createForm(ArticleEditType::class, $article);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $em->persist($article);
+            $em->flush();
+
+            return $this->redirect($this->generateUrl('article_view', [
+                'id' => $id
+            ]));
+        }
 
         return $this->render('edit/index.html.twig', [
             'form' => $form->createView(),
